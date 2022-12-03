51c4ce28bb8c265710ff5177ab444f3489c843f4
MMSSEM <106939626+mmssem@users.noreply.github.com>
2022-12-03 15:27:08 +0900

    n_01~03

--- www/data/login_ok.php
+++ www/data/login_ok.php
@@ -1,13 +1,15 @@
 <?php
     session_start();
 
-    if ( !isset($_POST['user_id']) || !isset($_POST['user_pw']) ) {
-        echo "<script>alert('아이디, 패스를 입력하세요.');";
-        echo "window.location.replace('login.php');</script>";
-        exit;
-    }
     $user_id = $_POST['user_id'];
     $user_pw = $_POST['user_pw'];
+
+    if ( $user_id == "" || $user_pw == "") {
+        echo "<script>alert('아이디, 패스를 입력하세요.');";
+        echo "window.location.replace('index.php');</script>";
+        exit;
+    }
+
     $members = array(
         'user' => array('password' => '1111', 'name' => '유저'),
         'khs' => array('password' => '2222', 'name' => '멍멍쌤')
@@ -15,7 +17,7 @@
  
     if( !isset($members[$user_id]) || $members[$user_id]['password'] != $user_pw ) {
         echo "<script>alert('아이디, 비밀번호를 확인하세요.');";
-        echo "window.location.replace('login.php');</script>";
+        echo "window.location.replace('index.php');</script>";
         exit;
     }
 
